112100a77b6c0dfddab970beb57dff681c5773f1
David2 <david2@gmail.com>
2021-11-09 19:52:57 +0100

    M09_Styles

--- web/user/register.php
+++ web/user/register.php
@@ -11,8 +11,13 @@
             </div>
 
             <div class='cajaLogin_Formulario'>
-                <label for="">Nombre</label><input type="text"></br>
-                <label for="">Nombre</label><input type="text">
+                <form action="">
+                    <label class='texto--negrita' for="name">Nombre</label><input class='formulario_nombre' name='name' type="text" placeholder='nombre' required></br></br>
+                    <label class='texto--negrita' for="">Email</label><input class='formulario_email' type="email" placeholder='email' required><br><br>
+                    <label class='texto--negrita' for="">Contraseña</label><input class='formulario_password' type="password" placeholder='password' required><br><br>
+                    <label class='texto--negrita' for="">Repetir Contraseña</label><input class='formulario_repPassword' type="email" placeholder='password' required><br><br>
+                    <button class='formulario_boton boton' type="submit">Registrarse</button>
+                </form>
             </div>
         </div>
 
