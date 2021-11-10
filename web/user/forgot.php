4e11fc1720b90c90e9edbbda6b7c776184923e8e
jorocaa <91740861+jorocaa@users.noreply.github.com>
2021-11-10 17:07:54 +0100

    Update forgot.php

--- web/user/forgot.php
+++ web/user/forgot.php
@@ -1,11 +1,16 @@
-<?php require_once __DIR__ . "/../vendor/autoload.php"; ?>
+<?php require_once __DIR__ . "/../../vendor/autoload.php"; ?>
 <!DOCTYPE html>
 <html lang="es">
-    <?= My\Helpers::render("/_commons/head.php", ["subtitle" => "Sign in"]); ?>
+    <?= My\Helpers::render("_commons/head.php", ["subtitle" => "Sign in"]); ?>
     <body>
-        <?= My\Helpers::render("/_commons/header.php"); ?>    
-        <h2>Sign in</h2>
-        <p>Welcom back!</p>
-        <?= My\Helpers::render("/_commons/footer.php"); ?>
+        <div class="parallax">
+            <form action="" class="form">
+                <h1 class="titulo">Has olvidado la contraseña?</h1><br>
+                <label for="email" class="form__label">E-mail: </label><br><input type="text" name="email" id="email" class="form__inputext"><br>
+                <label for="nombre" class="form__label">Nombre: </label><br><input type="text" name="nombre" id="nombre" class="form__inputext"><br>
+                <label for="contra" class="form__label">Última contraseña que recuerdes:</label><br><input type="text" name="contra" id="contra" class="form__inputext"><br>
+                <br><button class="boton">SEND</button>
+            </form>
+        </div>
     </body>
 </html>
