007f5e734d2be5e5bde9d1ef9d37f464658c76d9
jorocaa <91740861+jorocaa@users.noreply.github.com>
2021-11-10 17:08:37 +0100

    Update login.php

--- web/user/login.php
+++ web/user/login.php
@@ -1,11 +1,15 @@
-<?php require_once __DIR__ . "/../vendor/autoload.php"; ?>
+<?php require_once __DIR__ . "/../../vendor/autoload.php"; ?>
 <!DOCTYPE html>
 <html lang="es">
-    <?= My\Helpers::render("/_commons/head.php", ["subtitle" => "Sign in"]); ?>
-    <body>
-        <?= My\Helpers::render("/_commons/header.php"); ?>    
-        <h2>Sign in</h2>
-        <p>Welcom back!</p>
-        <?= My\Helpers::render("/_commons/footer.php"); ?>
+    <?= My\Helpers::render("_commons/head.php", ["subtitle" => "Sign in"]); ?>
+    <body>   
+        <div class="parallax">
+            <form action="" class="form">
+                <h1 class="titulo">LOG IN</h1>
+                <label for="nombre" class="form__label">Nombre: </label><br><input type="text" name="nombre" id="nombre" class="form__inputext"/><br>
+                <label for="contra" class="form__label">Contraseña: </label><br><input type="text" name="contra" id="contra" class="form__inputext"/><br>
+                <br><button class="boton">LOG IN</button>
+            </form>
+        </div>
     </body>
 </html>
