7b4bc7e9b875a5edb5f8d949e3224f465b009fcb
Drakno Rocha <joelrotete@gmail.com>
2021-12-17 19:18:25 +0100

    expires_action && messages flash added on PaginaInicio

--- web/user/expires_action.php
+++ web/user/expires_action.php
@@ -1,10 +1,28 @@
-<?php require_once __DIR__ . "/../../vendor/autoload.php";
+<?php
+
+use My\Helpers as Help;
+use My\Database as DB;
+
+require_once __DIR__ . "/../../vendor/autoload.php";
+
+session_start();
+
+/*******************/
+/* simulacre login */
+/*******************/
+// setcookie("session_token", "1234567890ABCDEFABCD12345678901234ABCDEF", time()+60000);
+// $_SESSION["uid"] = 2;
+/*******************/
+
 
 // COGEMOS TOKEN DE LA COOKIE
 $session_token = $_COOKIE["session_token"];
 
 // COGEMOS UID DE LA SESSION
 $uid = $_SESSION["uid"];
+
+// ABRIMOS BASE DE DATOS
+$connect = new DB();
 
 // MIRAMOS SI EL TOKEN EXISTE EN LA BASE DE DATOS
 $stmt = $connect->prepare("SELECT `token` 
@@ -21,15 +39,19 @@ foreach ($stmt as $row)
     }        
 }
 
+// CERRAMOS BASE DE DATOS
+$connect->close();
+
 // SI NO EXISTE EL TOKEN
 if(!$existe){
     $expires = time();
     setcookie($session_token, "",$expires,"","",false,false);
     unset($_SESSION["uid"]);
-}
+    Help::flash("Adiós.");
+}else{Help::flash("Todo en orden");}
 
 // REDIRIGIMOS A PÁGINA DE INICIO
-$url = My\Helpers::url("SuiteAplication.php");
-My\Helpers::redirect($url);
+$url = Help::url("PaginaInicio.php");
+Help::redirect($url);
 
 ?>
