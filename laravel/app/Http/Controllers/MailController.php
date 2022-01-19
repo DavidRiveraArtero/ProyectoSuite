e7e578426f0f9cf7224346195807837951663647
David2 <david2@gmail.com>
2022-01-19 19:19:16 +0100

    laravel email

---
+++ laravel/app/Http/Controllers/MailController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Mail\TestMail;
+
+class MailController extends Controller
+{
+    public function test(Request $request)
+    {
+        try
+        {
+            $mail = new TestMail([
+                'name' => 'Anonymous',
+                'body' => 'Testing mail',
+                'url' => '/'
+            ]);
+            \Mail::to('example@example.com')->send($mail);
+            echo '<h1>Mail send successfully</h1>';
+        }catch(\Expection $e)
+        {
+            echo '<pre>Error - ' .$e . '</pre>';
+        }
+    }
+}
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MailController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,5 @@ Route::get('/', function () {
 });
 
 Route::get('sendemail','SendEmailController@index')->name('sendemail');
+
+Route::get('mail/test',[MailController::class, 'test'])->name('enviar');
